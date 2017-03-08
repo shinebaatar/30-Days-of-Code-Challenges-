1578e41cf88784a4c947c4ba04250f951083c1c4
shinebaatar <b.shinebaatar@gmail.com>
2017-03-09 00:48:49 +0800

    updated
    Updated

--- src/Day15.java
+++ src/Day15.java
@@ -1,4 +1,3 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 
 class Node {
@@ -13,7 +12,19 @@ class Node {
 public class Day15 {
 	public static Node insert(Node head, int data){
 		Node node = new Node(data);
+
+		if(head == null){
+			head = node;
+			return head;
+		}
+		
 		Node temp = head;
+		while(temp.next != null){
+			temp = temp.next;
+		}
+		
+		temp.next = node;
+		
 		return head;
 	}
 	
---
+++ src/Day18.java
@@ -0,0 +1,6 @@
+
+public class Day18 {
+	public static void main(String[] args) {
+		
+	}
+}
